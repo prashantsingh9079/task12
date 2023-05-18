fb7a9d290f05b97e073d9c8b00ebd9a8ab1c28b8
prashantsingh9079 <prashant.singh9079@gmail.com>
2023-05-18 22:42:06 +0530

    added delete functionality

--- t12.js
+++ t12.js
@@ -1,5 +1,5 @@
 const form = document.getElementById("form-sb")
-console.log(form)
+//console.log(form)
 const name = document.getElementById("name1");
 const mail = document.getElementById("mail");
 const pn = document.getElementById("pn");
@@ -12,15 +12,27 @@ btn.addEventListener("click",addData)
 
 const arr=[]
 
+const uul = document.getElementById('divIn');
 
 function showOutput(res){
     var op='';
+    var dlt = document.createElement("button");
+    var edt = document.createElement("button");
+    edt.className="edt";
+    dlt.className="but"
+    dlt.appendChild(document.createTextNode("delete"));
+    edt.appendChild(document.createTextNode("edit"));
+    //var ss = document.createElement('li');
     for(let i=0; i<res.length ; i++)
     {
-        op+=`${res[i]['nv']}  ${res[i]['ev']}  ${res[i]['pv']}`+"<br> <hr>";
+        
+        op+=`<li id=${i}>${res[i]['nv']}  ${res[i]['ev']}  ${res[i]['pv']} <button class='buttontodel' type="button">Delete</button>  <button type="button">Edit</button></li>`+"<br> <hr>";
+        // document.getElementById(i).appendChild(dlt);
+        // document.getElementById(i).appendChild(edt);
+        
     }
-
-    document.body.innerHTML = document.body.innerHTML + op;
+    uul.innerHTML=op;
+    //document.body.innerHTML = document.body.innerHTML + op;
 
 }
 
@@ -51,7 +63,7 @@ function addData(e)
     var co = JSON.stringify(doo);
     var po ={nv,ev,pv}
     //localStorage.setItem(ev,co);
-    axios.post('https://crudcrud.com/api/44110acd0dac49efbf13bcd31e60b83a/info',po)
+    axios.post('https://crudcrud.com/api/e09ef8a991804471b2eee71c94d261d1/info/',po)
     .then((res)=>{
         
         console.log(res.data['nv'])
@@ -60,7 +72,7 @@ function addData(e)
         
         console.log(err.message);
     })
-    axios.get('https://crudcrud.com/api/44110acd0dac49efbf13bcd31e60b83a/info')
+    axios.get('https://crudcrud.com/api/e09ef8a991804471b2eee71c94d261d1/info/')
     .then((res)=>{
         console.log("data from get is ",res.data)
         showOutput(res.data)
@@ -114,5 +126,41 @@ function funtodel(e)
     var obj_s = JSON.stringify(obj)
     //console.log(obj_s)
     localStorage.removeItem(idddd.textContent)
+    }
+}
+
+const man = document.getElementById("divIn");
+console.log(man)
+man.addEventListener("click",delete_entry)
+
+function delete_entry(e)
+{
+    e.preventDefault();
+    var y;
+    if(e.target.className == 'buttontodel')
+    {
+        var xx = e.target.parentElement;
+        axios.get('https://crudcrud.com/api/e09ef8a991804471b2eee71c94d261d1/info/')
+        .then((res)=>{
+            
+            for(let i=0 ; i< res.data.length ;i++)
+            {
+                console.log("look here "+xx.textContent.split(" ")[3]," also ",res.data[i]['ev'])
+                if(res.data[i]['ev']== xx.textContent.split(" ")[3])
+                {
+                    y=res.data[i]['_id'];
+                    console.log("y is ",y)
+                    break;
+                }
+            }
+            axios.delete('https://crudcrud.com/api/e09ef8a991804471b2eee71c94d261d1/info/'+y).then((res)=>{
+                console.log("deleted ",res);
+            })
+        })
+        .catch((err)=>{
+            document.body.innerHTML = document.body.innerHTML + `<h3> something went wrong </h3>`;
+        })
+        man.removeChild(xx)
+        
     }
 }
